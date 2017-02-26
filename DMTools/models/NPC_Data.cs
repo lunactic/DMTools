aab4fba224db17721337813149c5639f83f7e884
Marcel Würsch <marcel.wuersch@unifr.ch>
2017-02-26 18:16:37 +0100

    updated

---
+++ DMTools/models/NPC_Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace WpfApplication1.models
+{
+    class NPC_Data
+    {
+        public List<Name> firstNames;
+        public List<Name> lastNames;
+        public List<Race> races;
+    }
+}
---
+++ DMTools/models/Name.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.models
+{
+    class Name
+    {
+        public string name;
+        public string race;
+    }
+}
